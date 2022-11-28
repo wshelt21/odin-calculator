fa095bd343f1aeed213e0e7c468db9128d96e576
wshelt21 <william.shelton24@gmail.com>
2022-11-28 13:10:25 -0500

    create button grid and basic layout of calculator

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 
+
+
+// Addition 
+function add (n1,n2){
+    return n1 + n2;
+}
+
+// Subtraction
+
+function subtract (n1,n2){
+    return n1 - n2;
+}
+
+// Divison
+function divide (n1,n2){
+    return n1 / n2;
+}
+
+
+// Multiplication
+function multiply (n1, n2){
+    return n1 * n2;
+}
+
+function operate (n1,n2){
+
+}
